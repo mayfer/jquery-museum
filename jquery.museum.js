592b549cc76b8f675728dbfa5f727883f365de6f
Murat Ayfer <murat@ayfer.net>
2014-01-11 20:37:57 -0800

    back button works, arrow keys work, everything works fyeah

--- jquery.museum.js
+++ jquery.museum.js
@@ -5,11 +5,13 @@
         var defaults = {
             namespace: 'msm',
             padding: 25,
+            disable_url_hash: false,
         }
 
         var plugin = this;
         plugin.settings = {}
         plugin.current_image = null;
+        plugin.loaded = false;
 
         var init = function() {
             plugin.settings = $.extend({}, defaults, options);
@@ -33,42 +35,68 @@
             $(document).keydown(function(e) {
                 if (e.keyCode == 27) {
                     plugin.close();
+                } else if(e.keyCode == 39) {
+                    // right
+                    plugin.next_image()
+                } else if(e.keyCode == 37) {
+                    // left
+                    plugin.prev_image()
                 }
             });
 
-            var plugin_hash ='#' + plugin.settings.namespace+'-gallery';
-            if(window.location.hash.substring(0, plugin_hash.length) === plugin_hash) {
-                var image_id = window.location.hash.split('-')[2];
-                plugin.show_gallery(image_id);
+            $(window).on('hashchange.'+plugin.settings.namespace, plugin.set_image_based_on_hash)
+            plugin.set_image_based_on_hash()
+
+        }
+
+        plugin.set_image_based_on_hash = function() {
+            if((window.location.hash == '' || window.location.hash == '#') && plugin.loaded === true) {
+                plugin.close();
+            } else {
+                var plugin_hash ='#' + plugin.settings.namespace+'-gallery';
+                if(window.location.hash.substring(0, plugin_hash.length) === plugin_hash) {
+                    var image_id = window.location.hash.split('-')[2];
+                    if(parseInt(image_id) != parseInt(plugin.current_image)) {
+                        plugin.show_gallery(image_id);
+                    }
+                }
             }
-            plugin.prev_hash = window.location.hash;
         }
 
         plugin.show_gallery = function(i) {
-            plugin.container = $('<div>').addClass(plugin.settings.namespace + '-gallery').appendTo($('body'));
-            plugin.container.css({
-                'position': 'absolute',
-                'top': '0',
-                'left': '0',
-                'width': $(document).width() + 'px',
-                'height': $(document).height() + 'px',
-                'background': 'rgba(0, 0, 0, 0.7)',
-                'text-align': 'center',
-            });
-            plugin.container.click(function(e){
-                plugin.close();
-            });
-            plugin.content = $('<div>').addClass(plugin.settings.namespace + '-content');
+            if(plugin.loaded === false) {
+                plugin.container = $('<div>').addClass(plugin.settings.namespace + '-gallery').appendTo($('body'));
+                plugin.container.css({
+                    'position': 'absolute',
+                    'top': '0',
+                    'left': '0',
+                    'width': $(document).width() + 'px',
+                    'height': $(document).height() + 'px',
+                    'background': 'rgba(0, 0, 0, 0.7)',
+                    'text-align': 'center',
+                });
+                plugin.container.click(function(e){
+                    plugin.close();
+                });
+                plugin.content = $('<div>').addClass(plugin.settings.namespace + '-content');
 
 
-            plugin.content.css({
-                'visibility': 'invisible',
+                plugin.content.css({
+                    'visibility': 'invisible',
+                });
+                plugin.loaded = true;
+            }
+            plugin.content.on('click', 'img', function(e) {
+                e.stopPropagation();
+                plugin.next_image();
             });
             plugin.show_image(i);
         }
 
         plugin.show_image = function(i) {
-            window.location.hash = plugin.settings.namespace + '-gallery-' + i;
+            if(plugin.settings.disable_url_hash !== true) {
+                window.location.hash = plugin.settings.namespace + '-gallery-' + i;
+            }
 
             plugin.current_image = i;
             plugin.content.empty();
@@ -83,13 +111,8 @@
                     'background': 'rgba(0,0,0,0.6)',
                 })
                 .attr('src', plugin.images[i].src)
-                .appendTo(plugin.content)
-                .click(function(e) {
-                    e.stopPropagation();
-                    plugin.next_image();
-                });
+                .appendTo(plugin.content);
             plugin.show_image_when_available();
-
         }
 
         plugin.show_image_when_available = function() {
@@ -121,8 +144,14 @@
             plugin.show_image((plugin.current_image + 1) % plugin.images.length);
         }
 
+        plugin.prev_image = function() {
+            var index = (plugin.images.length + plugin.current_image - 1) % plugin.images.length;
+            plugin.show_image(index);
+        }
+
         plugin.close = function() {
-            window.location.hash = plugin.prev_hash;
+            plugin.loaded = false;
+            window.location.hash = '';
             plugin.container.remove();
         }
 
